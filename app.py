b9c4b70eef6f3a8f181ac87a3c3a5d3e69ff2135
Bünyamin Önüm <59376910+bunyaminonum@users.noreply.github.com>
2024-11-06 16:28:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("Hello from OpenShift!")
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import os
+
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
+    port = int(os.environ.get("PORT", 8080))
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Starting httpd server on port {port}")
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
